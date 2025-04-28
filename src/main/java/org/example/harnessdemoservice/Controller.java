1424e6cc8c5d642babec47081750def5cbb4dee9
karthikmurugiah <karthivkmurugiah@gmail.com>
2025-04-28 17:13:05 +0530

    changes to service

--- src/main/java/org/example/harnessdemoservice/Controller.java
+++ src/main/java/org/example/harnessdemoservice/Controller.java
@@ -1,14 +1,16 @@
 package org.example.harnessdemoservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@RequestMapping("/harness")
 public class Controller {
 
-    @RequestMapping("/")
+    @GetMapping
     public String home() {
         log.debug("Tracing..");
         return "Done!";
